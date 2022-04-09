fac37cdf4faa4a03601f0b1a6de5f1d201981ce7
EricoDeMecha <ford.koech.eric@gmail.com>
2022-04-09 15:19:57 +0300

    add vscode integrations of conan

--- conanfile.py
+++ conanfile.py
@@ -56,7 +56,7 @@ class OpenCVConan(ConanFile):
         "shared": False,
         "fPIC": True,
         "parallel": False,
-        "contrib": True,
+        "contrib": False,
         "contrib_freetype": False,
         "contrib_sfm": False,
         "with_ipp": False,
